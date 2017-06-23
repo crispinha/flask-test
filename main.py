9c3a929f0294a9b07b14344858d9979061d372d5
Crispin <crispin@developster.io>
2017-06-23 19:19:45 +1200

    Messages are now a list.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-user_message = ""
+user_message = []
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -9,12 +9,14 @@ def message():
 	global user_message
 	if request.method == 'POST':
 		if "message" in request.form:
-			user_message = request.form["message"]
+			user_message.append(request.form["message"])
 			return redirect(url_for('message'))
 		else:
 			user_message = user_message
 			raise KeyError
 	return render_template("index.html", message=user_message)
+
+#change itt
 
 if __name__ == "__main__":
 	app.run()
