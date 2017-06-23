e95d512983e2d14dd79f53702348a46a8df9fb78
Crispin <crispin@developster.io>
2017-06-24 09:48:14 +1200

    cookies?

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort, make_response
+import random
 app = Flask(__name__)
 
 user_message = []
@@ -12,11 +13,21 @@ def message():
 			user_message.append(request.form["message"])
 			return redirect(url_for('message'))
 		else:
-			user_message = user_message
-			raise KeyError
+			abort(422)
+
 	return render_template("index.html", message=user_message)
 
-#change itt
+
+@app.route("/set_cookie/")
+def set_cookie():
+	resp = make_response(render_template("done.html"))
+	resp.set_cookie("number", str(random.randint(0, 100000)))
+	return resp
+
+
+@app.route("/get_cookie/")
+def get_cookie():
+	return render_template("show_cookie.html", number=request.cookies.get('number'))
 
 if __name__ == "__main__":
 	app.run()
